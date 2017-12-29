671353fb3e1d4a74c0c8b1d684145da351009684
WENGWENYU\wengw <wengwenyu@aliyun.com>
2017-12-29 23:34:58 +0800

    测试模块提交

---
+++ 2017年老代码存放区域/备课/面向对象/测试模块.py
@@ -0,0 +1,13 @@
+#def add(a,b):
+#    return a+b
+#
+##用来测试
+#ret = add(12,22)
+#print('int test.py file,,,,,,12+12=%d'%ret)
+#
+def add(a,b):
+    return a+b
+
+#用来测试
+ret = add(12,22)
+print('in test.py file,__name__ is %s'%__name__)
