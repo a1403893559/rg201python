6d554bdfe95c72eabc86dc34f45611fff8c48baf
WENGWENYU\wengw <wengwenyu@aliyun.com>
2017-12-29 09:29:40 +0800

    素数

---
+++ 2017年老代码存放区域/考试题部分代码/求1_100之间的素数.py
@@ -0,0 +1,15 @@
+# 求1-100之间的素数
+
+L = []
+for i in range(2,101):
+       mark = 0
+    for j in range(2,i-1):
+        if i%j == 0:
+            print('不是素数')
+            mark = 1
+    if mark == 0:
+        L.append(i)
+
+        
+     
+        
