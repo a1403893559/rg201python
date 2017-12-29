299b820c9253321afe3a4dd0772f0f0677d121fe
WENGWENYU\wengw <wengwenyu@aliyun.com>
2017-12-29 10:44:33 +0800

    求100到200间的素数

--- 2017年老代码存放区域/考试题部分代码/求1_100之间的素数.py
+++ 2017年老代码存放区域/考试题部分代码/求1_100之间的素数.py
@@ -1,14 +1,36 @@
-# 求1-100之间的素数
-
+# 求100-200之间的素数
 L = []
-for i in range(2,101):
+for i in range(100,201):
     mark = 0
     for j in range(2,i):
         if i%j == 0:
             mark = 1
+            break
     if mark == 0:
         L.append(i)
 
 print(L)
-     
+        
+
+
+
+
+
+# 求1-100之间的素数
+
+# L = []
+# for i in range(2,101):
+#     mark = 0
+#     for j in range(2,i):
+#         if i%j == 0:
+#             mark = 1
+#     if mark == 0:
+#         L.append(i)
+
+# print(L)
+
+
+
+
+
         
