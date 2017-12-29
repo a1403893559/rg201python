b5c5e6fea051d9df90b307788dbb7fbc3fae665f
WENGWENYU\wengw <wengwenyu@aliyun.com>
2017-12-29 19:44:14 +0800

    提交

--- 2017年老代码存放区域/备课/面向对象/new方法.py
+++ 2017年老代码存放区域/备课/面向对象/new方法.py
@@ -1,8 +1,12 @@
 class A(object):
     def __init__(self):
+        print(self)
         print('这是init方法')
     def __new__(cls):
+        print(id(cls))
         print('这是new方法')
-        return object.__new__(cls)
-
-A()
+        ret = object.__new__(cls)
+        return ret
+a = A()
+print(a)
+print(id(A))
