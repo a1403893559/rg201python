19072260cd8621005018585b3cd9c7699cf7cd54
WENGWENYU\wengw <wengwenyu@aliyun.com>
2018-01-01 23:20:26 +0800

    4s店

---
+++ 2017年老代码存放区域/备课/面向对象/设计卖车的4S店.py
@@ -0,0 +1,31 @@
+# class Car(object):
+#     # 定义车的方法
+#     def move(self):
+#         print('移动')
+    
+#     def stop(self):
+#         print('停车')
+    
+# # 定义一个销售车的店类
+# class CarStore(object):
+
+#     def order(self):
+#         self.car = Car()# 找到一辆车
+#         self.car.move()
+#         self.car.stop()
+
+
+# buy_car = CarStore()
+# buy_car.order()
+
+class BMW(object):
+    def move(self):
+        print('车在移动')
+    def stop(self):
+        print('停车')
+
+class BC(object):
+    def move(self):
+        print('车在移动')
+    def stop(self):
+        print('停车')
