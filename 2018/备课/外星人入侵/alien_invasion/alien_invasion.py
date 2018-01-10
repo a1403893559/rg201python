5bd2e17160e9f005156c64c8e2926e421ae761fd
wengwenyu <wengwenyu@aliyun.com>
2018-01-10 22:57:16 +0800

    创建pygame窗口

---
+++ 2018/备课/外星人入侵/alien_invasion/alien_invasion.py
@@ -0,0 +1,23 @@
+import sys
+
+import pygame
+
+def run_game():
+    # 初始化游戏并且创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode(1200,800)
+    pygame.display.set_caption('外星人入侵')
+
+    # 开始游戏主循环
+    while True:
+
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
+
