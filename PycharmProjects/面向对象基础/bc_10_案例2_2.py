0119ced9d211817153cc5c85f6d88a6c3aa65543
WENGWENYU\wengw <wengwenyu@aliyun.com>
2018-03-02 16:28:39 +0800

    提交更新

--- PycharmProjects/面向对象基础/bc_10_案例2_2.py
+++ PycharmProjects/面向对象基础/bc_10_案例2_2.py
@@ -1,3 +1,15 @@
+class HouseItem:
+    def __init__(self,name,area):
+        """
+        :param name: 家具名称
+        :param area: 占地面积
+        """
+        self.name = name
+        self.area = area
+
+    def __str__(self):
+        return "[%s]占地面积%.2f"%(self.name,self.area)
+
 class House:
     def __init__(self,house_type,area):
         """
@@ -18,11 +30,15 @@ class House:
     def add_item(self,item):
         print("要添加%s"%item)
 
+# 1.创建家具
+bed = HouseItem("席梦思", 4)
+chest = HouseItem("衣柜", 2)
+table = HouseItem("餐桌", 1.5)
 
-# 创建房子对象
+# 2.创建房子对象
 my_home = House("两室一厅",60)
-my_home.add_item("bed")
-my_home.add_item("chest")
-my_home.add_item("table")
+my_home.add_item(bed)
+my_home.add_item(chest)
+my_home.add_item(table)
 print(my_home)
 
